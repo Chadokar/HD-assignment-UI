953687baf2ff65e4de61942673469b02d6e885cc
Shubham Chadokar <shubhamchadokar36@gmail.com>
2025-01-26 04:45:10 +0530

    minor updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/HD-assignment-UI",
+  base: "/HD-assignment-UI",
   optimizeDeps: {
     include: ["lucide-react"],
   },
